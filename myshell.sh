9a71503632bdd4cc14b5958124cec38899a4e8dd
rezanaemi <47867297+rezanaemi@users.noreply.github.com>
2019-03-07 19:43:12 +0200

    HTML/CSS W0
    MY SHELL

---
+++ myshell.sh
@@ -0,0 +1,16 @@
+cd C:/Users/SHA-31/Desktop/
+mkdir class6
+cd ./class6
+touch my1.txt
+touch my2.txt
+touch my3.txt
+touch my4.txt
+mkdir file1
+mkdir file2
+mkdir file3 
+cd ./file1
+touch my1.txt
+touch my2.txt
+touch my3.txt
+touch my4.txt
+
